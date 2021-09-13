e25c98f1cc1f708373881ee4fcd5d3f9cdaa3765
Tim Davis <contact@timdav.is>
2021-09-13 01:22:11 +0000

    use default dpkg options

--- .devcontainer/scripts/base.sh
+++ .devcontainer/scripts/base.sh
@@ -6,28 +6,26 @@ set -e
 
 apt-get update
 
-apt-get -y -o Dpkg::Options::="--force-confdef" \
-           -o Dpkg::Options::="--force-confold" \
-  install \
-    bash-completion \
-    build-essential \
-    curl \
-    dirmngr \
-    git \
-    gnupg \
-    host \
-    htop \
-    iputils-ping \
-    locales \
-    man \
-    nano \
-    netcat-openbsd \
-    psmisc \
-    sudo \
-    traceroute \
-    tzdata \
-    unzip \
-    wget
+apt-get -y install \
+  bash-completion \
+  build-essential \
+  curl \
+  dirmngr \
+  git \
+  gnupg \
+  host \
+  htop \
+  iputils-ping \
+  locales \
+  man \
+  nano \
+  netcat-openbsd \
+  psmisc \
+  sudo \
+  traceroute \
+  tzdata \
+  unzip \
+  wget
 
 apt-get clean -y
 rm -rf /var/lib/apt/lists/*
