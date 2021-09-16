a5fcc3e4e8d9d1402d2d3e396af0b6cf7c10b30d
Tim Davis <contact@timdav.is>
2021-09-16 04:01:12 +0000

    expand base deps

--- .devcontainer/scripts/base.sh
+++ .devcontainer/scripts/base.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US:en'
+export LC_ALL='en_US.UTF-8'
 
 set -e
 
 apt-get update
 
 apt-get -y install \
+  apt-transport-https \
   bash-completion \
   build-essential \
   curl \
@@ -17,6 +21,7 @@ apt-get -y install \
   htop \
   iputils-ping \
   locales \
+  lsb-release \
   man \
   nano \
   netcat-openbsd \
@@ -24,8 +29,10 @@ apt-get -y install \
   sudo \
   traceroute \
   tzdata \
-  unzip \
-  wget
+  unzip
+
+apt-get autoremove
 
 apt-get clean -y
+
 rm -rf /var/lib/apt/lists/*
