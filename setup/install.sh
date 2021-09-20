67e11fbf9d0c78db4cb1199b98503afcd344849d
Tim Davis <contact@timdav.is>
2021-09-20 07:56:04 -0700

    remove bad flag from creating vscode user

--- setup/install.sh
+++ setup/install.sh
@@ -158,7 +158,7 @@ EOF
 echo 'vscode ALL=(dev) NOPASSWD: /bin/bash' > /etc/sudoers.d/vscode
 chmod 440 /etc/sudoers.d/vscode
 
-useradd -s /bin/bash -G vscode
+useradd -s /bin/bash vscode
 
 chown -R vscode:vscode /home/vscode
 
