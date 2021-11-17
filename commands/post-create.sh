c4bf6e51403246a285b5f554d517709041da065f
Tim Davis <contact@timdav.is>
2021-11-17 15:09:12 -0800

    update post-create to set acl for workspace folder

--- commands/post-create.sh
+++ commands/post-create.sh
@@ -3,12 +3,17 @@
 # fix permissions so that dev and vscode can write each other's files
 
 if [ -n "$WORKSPACE_FOLDER" ] && [ -d "$WORKSPACE_FOLDER" ]; then
+
   workspaces="$(dirname "$WORKSPACE_FOLDER")"
   workspace_basename="$(basename "$WORKSPACE_FOLDER")"
-  if [ ! -f "$workspaces"/."$workspace_basename" ]; then
+  workspace_permissions_set="$workspaces"/."$workspace_basename"
+
+  if [ ! -f "$workspace_permissions_set" ]; then
     chmod g+rwx "$WORKSPACE_FOLDER"
     find "$WORKSPACE_FOLDER" -type f -exec chmod g+rw {} \;
     find "$WORKSPACE_FOLDER" -type d -exec chmod g+rwx {} \;
-    touch "$workspaces"/."$workspace_basename"
+    setfacl -Rm d:g:vscode:rw,g:vscode:rw "$WORKSPACE_FOLDER"
+    touch "$workspace_permissions_set"
   fi
+
 fi
