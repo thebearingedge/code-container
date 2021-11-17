70e0b7fbe78cc29b2a664268d4d799b277856be7
Tim Davis <contact@timdav.is>
2021-11-17 14:30:26 -0800

    set default pam umask and fix project mount permissions

--- commands/post-create.sh
+++ commands/post-create.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# fix permissions so that dev and vscode can write each other's files
+
 if [ -n "$WORKSPACE_FOLDER" ] && [ -d "$WORKSPACE_FOLDER" ]; then
-  chmod g+rwx "$WORKSPACE_FOLDER"
-  find "$WORKSPACE_FOLDER" -type f -exec chmod g+rw {} \;
-  find "$WORKSPACE_FOLDER" -type d -exec chmod g+rwx {} \;
+  workspaces="$(dirname "$WORKSPACE_FOLDER")"
+  workspace_basename="$(basename "$WORKSPACE_FOLDER")"
+  if [ ! -f "$workspaces"/."$workspace_basename" ]; then
+    chmod g+rwx "$WORKSPACE_FOLDER"
+    find "$WORKSPACE_FOLDER" -type f -exec chmod g+rw {} \;
+    find "$WORKSPACE_FOLDER" -type d -exec chmod g+rwx {} \;
+    touch "$workspaces"/."$workspace_basename"
+  fi
 fi
--- home/dev/.bash_profile.d/cd_workspace_folder.bash
+++ home/dev/.bash_profile.d/cd_workspace_folder.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 if [ -n "$WORKSPACE_FOLDER" ] && [ -d "$WORKSPACE_FOLDER" ]; then
-  umask 0002
   cd "$WORKSPACE_FOLDER" || return
 fi
